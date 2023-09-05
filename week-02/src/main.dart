9d44e9cbc4578ec5a51bc9821a45dcb60aefdd02
Patria Anggara Susilo Putra <susiloangga17@gmail.com>
2023-09-05 18:59:30 +0700

    week 02 - fiks#2

--- week-02/src/main.dart
+++ week-02/src/main.dart
@@ -1,6 +1,5 @@
 void main() {
-  for (int i = 0; i < 10; i++) {
-    print("Nama saya adalah Angga, sekarang berumur ${i + 2}");
+  for (int i = 20; i >= 11; i--) {
+    print("Nama saya adalah Angga, sekarang berumur ${i - 2}");
   }
 }
-
