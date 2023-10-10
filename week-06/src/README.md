6617e5e99c6456f5df980b5140cb2e090121918b
Patria Anggara Susilo Putra <susiloangga17@gmail.com>
2023-10-10 15:14:08 +0700

    week 06 - praktikum 1 langkah 1

--- week-06/src/README.md
+++ week-06/src/README.md
@@ -1,4 +1,4 @@
-# src
+# layout_flutter
 
 A new Flutter project.
 
--- week-06/src/android/app/src/main/kotlin/com/example/layout_flutter/MainActivity.kt
+++ week-06/src/android/app/src/main/kotlin/com/example/layout_flutter/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.src
+package com.example.layout_flutter
 
 import io.flutter.embedding.android.FlutterActivity
 
