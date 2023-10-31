5b1ef335af6595b219408727820be29e05448a48
Patria Anggara Susilo Putra <susiloangga17@gmail.com>
2023-10-31 16:44:14 +0700

    week 09 - praktikum langkah 7

--- week-09/src/lib/main.dart
+++ week-09/src/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:src/red_text_widget.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +16,29 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: null,
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                color: Colors.yellowAccent,
+                width: 50,
+                child: const RedTextWidget(
+                  text: 'You have pushed the button this many times:',
+                ),
+              ),
+              Container(
+                color: Colors.greenAccent,
+                width: 100,
+                child: const Text(
+                  'You have pushed the button this many times:',
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
