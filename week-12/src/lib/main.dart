2e21a313130da1610e04253332aebfe2b034cf03
Patria Anggara Susilo Putra <susiloangga17@gmail.com>
2023-11-14 15:52:12 +0700

    week 12 - praktikum 2

--- week-12/src/lib/main.dart
+++ week-12/src/lib/main.dart
@@ -43,18 +43,19 @@ class _FuturePageState extends State<FuturePage> {
           children: [
             const Spacer(),
             ElevatedButton(
-              onPressed: () {
-                setState( () {
-                  getData().then( (value) {
-                    result = value.body.toString().substring(0, 450);
-                    setState( () {} );
-                  } ).catchError( (_) {
-                    result = "An error occured";
-                    setState( () {} );
-                  } );
-                } );
-              },
               child: const Text("GO!"),
+              onPressed: () {
+                count();
+                // setState( () {
+                //   getData().then( (value) {
+                //     result = value.body.toString().substring(0, 450);
+                //     setState( () {} );
+                //   } ).catchError( (_) {
+                //     result = "An error occured";
+                //     setState( () {} );
+                //   } );
+                // } );
+              },
             ),
             const Spacer(),
             Text(result),
@@ -72,6 +73,31 @@ class _FuturePageState extends State<FuturePage> {
     const path = "/books/v1/volumes/J69lDwAAQBAJ";
     Uri url = Uri.https(authority, path);
     return http.get(url);
+  }
+
+  Future<int> returnOneAsync() async {
+    await Future.delayed(const Duration(seconds: 3));
+    return 1;
+  }
+
+  Future<int> returnTwoAsync() async {
+    await Future.delayed(const Duration(seconds: 3));
+    return 2;
+  }
+
+  Future<int> returnThreeAsync() async {
+    await Future.delayed(const Duration(seconds: 3));
+    return 3;
+  }
+
+  Future count() async {
+    int total = 0;
+    total = await returnOneAsync();
+    total += await returnTwoAsync();
+    total += await returnThreeAsync();
+    setState(() {
+      result = total.toString();
+    });
   }
 }
 
