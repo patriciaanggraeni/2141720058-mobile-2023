a2010aa4b8d782bb596bfe31324b3f1051fd267e
Patria Anggara Susilo Putra <susiloangga17@gmail.com>
2023-11-14 15:26:53 +0700

    week 12 - praktikum 1

---
+++ week-12/README.md
@@ -0,0 +1,19 @@
+## Nama: Patria Anggara Susilo Putra
+## NIM : 2141720058
+## Kelas: 3F
+
+---
+
+### Praktikum 1
+
+1. Mengganti title dengan nama panggilan.
+!["screenshot_praktikum_satu](docs/images/praktikum1/soal/soal1.png)
+
+2. Mencari judul favorit di Google Books dan menampilkan data JSON-nya.
+!["screenshot_praktikum_satu](docs/images/praktikum1/soal/soal2.png)
+
+3. Maksud kode **subString** dan **catchError**.
+- subString: mengambil jumlah karakter string dari range tertentu, di sini dari karaketer ke-0 sampai karakter ke-450.
+- catchError: jika terjadi error saat menekan tombol GO, maka block catchError akan dieksekusi dan menampilkan pesan "An error Accurred".
+
+!["screenshot_praktikum_satu](docs/images/praktikum1/soal/demo.gif)
---
+++ week-12/src/lib/main.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Angga',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const FuturePage(),
+    );
+  }
+}
+
+class FuturePage extends StatefulWidget {
+  const FuturePage({super.key});
+
+  @override
+  State<FuturePage> createState() => _FuturePageState();
+}
+
+class _FuturePageState extends State<FuturePage> {
+  String result = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Back from the Future"),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            const Spacer(),
+            ElevatedButton(
+              onPressed: () {
+                setState( () {
+                  getData().then( (value) {
+                    result = value.body.toString().substring(0, 450);
+                    setState( () {} );
+                  } ).catchError( (_) {
+                    result = "An error occured";
+                    setState( () {} );
+                  } );
+                } );
+              },
+              child: const Text("GO!"),
+            ),
+            const Spacer(),
+            Text(result),
+            const Spacer(),
+            const CircularProgressIndicator(),
+            const Spacer(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<Response> getData() async {
+    const authority = "www.googleapis.com";
+    const path = "/books/v1/volumes/J69lDwAAQBAJ";
+    Uri url = Uri.https(authority, path);
+    return http.get(url);
+  }
+}
+
