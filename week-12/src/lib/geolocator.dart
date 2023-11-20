06bd8b3ed810c38fdf4fca93114ba05002eb6494
Patria Anggara Susilo Putra <susiloangga17@gmail.com>
2023-11-20 18:35:01 +0700

    week 12 - praktikum 6 [ada yang kelewat]

--- week-12/src/lib/geolocator.dart
+++ week-12/src/lib/geolocator.dart
@@ -10,10 +10,12 @@ class LocationScreen extends StatefulWidget {
 
 class _LocationScreenState extends State<LocationScreen> {
   String myPosition = "";
+  Future<Position>? position;
 
   @override
   void initState() {
     super.initState();
+    // position = getPosition();
     getPosition().then((Position myPos) {
       myPosition = "Latitude: ${myPos.latitude.toString()} - "
           "Longitude: ${myPos.longitude.toString()}";
@@ -25,22 +27,54 @@ class _LocationScreenState extends State<LocationScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final myWidget = myPosition == ""
+    ? const CircularProgressIndicator()
+    : Text(myPosition);
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Patria Anggara Susilo Putra"
-            " - Current Location"),
+        title: const Text("Patria Anggara Susilo Putra - Current Location"),
       ),
       body: Center(
-        child: Text(myPosition),
+        // child: Container(myWidget,),
+        child: FutureBuilder(
+          future: position,
+          builder: (BuildContext context, AsyncSnapshot<Position> snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const CircularProgressIndicator();
+            } else if (snapshot.connectionState == ConnectionState.done) {
+              if (snapshot.hasError) {
+                 return const Text('Something terrible happened!');
+              }
+              return Text(snapshot.data.toString());
+            } else {
+              return const Text("");
+            }
+
+            // if (snapshot.connectionState == ConnectionState.waiting) {
+            //   return const CircularProgressIndicator();
+            // } else if (snapshot.connectionState == ConnectionState.done) {
+            //   return Text(snapshot.data.toString());
+            // } else {
+            //   return const Text("");
+            // }
+          },
+        ),
       ),
     );
-
   }
 
+  // Future<Position> getPosition() async {
+  //   await Geolocator.requestPermission();
+  //   await Geolocator.isLocationServiceEnabled();
+  //   Position? position = await Geolocator.getCurrentPosition();
+  //   return position;
+  // }
+
   Future<Position> getPosition() async {
-    await Geolocator.requestPermission();
     await Geolocator.isLocationServiceEnabled();
-    Position? position = await Geolocator.getCurrentPosition();
+    await Future.delayed(const Duration(seconds: 3));
+    Position position = await Geolocator.getCurrentPosition();
     return position;
   }
 }
