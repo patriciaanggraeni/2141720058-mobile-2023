7350851dba034a77ca1d32c541a23baf1037ea73
Patria Anggara Susilo Putra <susiloangga17@gmail.com>
2023-11-21 15:09:32 +0700

    week 13 - Jawaban Soal 2

---
+++ week-13/src/state_management_advance/lib/stream.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ColorStream {
+  final List<Color> colors = [
+    Colors.blueGrey,
+    Colors.amber,
+    Colors.deepPurple,
+    Colors.lightBlue,
+    Colors.teal,
+
+    // Jawaban Soal Nomor 2 Point Pertama
+    Colors.lime,
+    Colors.pink,
+    Colors.black12,
+    Colors.cyan,
+    Colors.indigo,
+  ];
+}
