3d9662b16f602c2c54c2c2d91b0636b567750af2
Patria Anggara Susilo Putra <susiloangga17@gmail.com>
2023-11-27 22:48:30 +0700

    week 13 - Jawaban Soal 13

--- week-13/src/bloc_pattern/lib/main.dart
+++ week-13/src/bloc_pattern/lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:bloc_pattern/random_bloc.dart';
+import 'package:bloc_pattern/random_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,12 +12,22 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Bloc Pattern',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        primarySwatch: Colors.deepPurple,
         useMaterial3: true,
       ),
-      home: null,
+      home: const RandomScreen(),
     );
   }
 }
+
+/*
+  Jawaban Soal 13 Point Pertama:
+    - Maksudnya adalah membuat pattern bloc pada aplikasi yang mana nanti fungsinya akan berdampak
+      jika aplikasinya cukup besar, saat membuat class RandomBloc, class tersebut merupakan bloc
+      dan saat ingin menghubungkan dengan class RandomScreen, sebelumnya mendeklarasikan variabel
+      _blocRandomNumber, variabel inilah yang menghubungkan class bloc dengan pengguna class bloc
+      tersebut.
+*/
+
